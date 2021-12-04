81c8dc49a46563c95da25e7b28954a181aaf3145
KathrinBeaver <kathrin.beaver@mail.ru>
2021-12-04 07:58:25 +0300

    Add readme

--- src/main/java/ru/mai/springboot/restful/product/Product.java
+++ src/main/java/ru/mai/springboot/restful/product/Product.java
@@ -12,17 +12,13 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 @Entity
-
 @Data
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
     private String description;
-
     private BigDecimal price;
 
     @CreationTimestamp
